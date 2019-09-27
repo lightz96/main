18234a0d76f291deb4b71af3e0b83a68074c7e99
boqiang <bobo_qiang@hotmail.com>
2019-09-27 20:30:55 +0800

    Remove Person class

--- src/main/java/thrift/model/ModelManager.java
+++ src/main/java/thrift/model/ModelManager.java
@@ -124,7 +124,7 @@ public class ModelManager implements Model {
     //=========== Filtered Transaction List Accessors =============================================================
 
     /**
-     * Returns an unmodifiable view of the list of {@code Person} backed by the internal list of
+     * Returns an unmodifiable view of the list of {@code Transaction} backed by the internal list of
      * {@code versionedAddressBook}
      */
     @Override
