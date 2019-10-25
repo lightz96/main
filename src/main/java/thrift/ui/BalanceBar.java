99d713b823a5809705bfabc6d39961837978c8d5
boqiang <bobo_qiang@hotmail.com>
2019-10-25 16:04:55 +0800

    Add cards to the UI

--- src/main/java/thrift/ui/BalanceBar.java
+++ src/main/java/thrift/ui/BalanceBar.java
@@ -13,39 +13,39 @@ public class BalanceBar extends UiPart<Region> {
 
     private static final String FXML = "BalanceBar.fxml";
 
-    @FXML
-    private Label monthYearLabel;
+//    @FXML
+//    private Label monthYearLabel;
 
-    @FXML
-    private Label monthBudgetLabel;
-
-    @FXML
-    private Label monthBudgetValueLabel;
-
-    @FXML
-    private Label monthBalanceLabel;
-
+//    @FXML
+//    private Label monthBudgetLabel;
+//
+//    @FXML
+//    private Label monthBudgetValueLabel;
+//
+//    @FXML
+//    private Label monthBalanceLabel;
+//
     @FXML
     private Label monthBalanceValueLabel;
 
     public BalanceBar(String monthYear, double monthBudget, double monthBalance) {
         super(FXML);
         setMonthYear(monthYear);
-        monthBudgetLabel.setText("Budget: $");
+//        monthBudgetLabel.setText("Budget: $");
         setMonthBudget(monthBudget);
-        monthBalanceLabel.setText("\tBalance: ");
+//        monthBalanceLabel.setText("\tBalance: ");
         setMonthBalance(monthBalance);
 
-        monthBudgetLabel.setWrapText(true);
+//        monthBudgetLabel.setWrapText(true);
         monthBalanceValueLabel.setWrapText(true);
     }
 
     public void setMonthYear(String monthYear) {
-        monthYearLabel.setText(monthYear);
+//        monthYearLabel.setText(monthYear);
     }
 
     public void setMonthBudget(double monthBudget) {
-        monthBudgetValueLabel.setText(String.valueOf(DECIMAL_FORMATTER.format(monthBudget)));
+//        monthBudgetValueLabel.setText(String.valueOf(DECIMAL_FORMATTER.format(monthBudget)));
     }
 
     public void setMonthBalance(double monthBalance) {
@@ -55,7 +55,7 @@ public class BalanceBar extends UiPart<Region> {
             monthBalanceValueLabel.setStyle("-fx-text-fill: #ff6c4f;");
         } else {
             sb.append("$").append(DECIMAL_FORMATTER.format(monthBalance));
-            monthBalanceValueLabel.setStyle("-fx-text-fill: #69ff4f;");
+            monthBalanceValueLabel.setStyle("-fx-text-fill: #4CAF50;");
         }
         monthBalanceValueLabel.setText(sb.toString());
     }
